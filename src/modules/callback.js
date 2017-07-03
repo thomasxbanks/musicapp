74cb35bd168e49fa2cee3eeb15f1baf5bf429735
thomasxbanks <thomasxbanks@gmail.com>
2017-07-03 08:04:25 +0100

    callback test endpoint added

--- src/modules/callback.js
+++ src/modules/callback.js
@@ -1,12 +1,30 @@
 let request = require('request')
 
 module.exports = function(req, res) {
-	console.log(req)
-	let data = {
-		page: 'callback',
-		template: 'index',
-		title: 'fuck',
-		content: `your mum`
-	}
-	res.render(data.template, data)
+	console.log("REQUEST\n******************************************************", req)
+	console.log("RESPONSE\n*****************************************************", res)
+	let endpoint = 'http://scrummable.com/wp-json/wp/v2/posts?_embed'
+	request(endpoint, function(error, response, body) {
+		console.log(response.statusCode, body)
+		if (response.statusCode <= 200) {
+			let data = {
+				page: 'callback',
+				template: 'index',
+				title: 'fuck',
+				content: `your mum`
+			}
+			res.render(data.template, data)
+
+		} else {
+			console.log('error:', body)
+			data.content = {
+				statusCode: response.statusCode,
+				message: body
+			}
+			console.log("**********\n", data)
+			res.render(data.template, data)
+
+		}
+	})
+
 }
